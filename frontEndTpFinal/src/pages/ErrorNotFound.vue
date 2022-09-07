3b7551d572615b2e3f3ae861109b41c2a379177c
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2022-09-07 01:50:33 -0300

    Agregando Form

--- frontEndTpFinal/src/pages/ErrorNotFound.vue
+++ frontEndTpFinal/src/pages/ErrorNotFound.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-indigo text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
         404
       </div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+      <div class="text-h2" style="opacity:.5">
+        Nada por aquí, nada por allá
       </div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="indigo"
         unelevated
         to="/"
-        label="Go Home"
+        label="Volvamos"
         no-caps
       />
     </div>
