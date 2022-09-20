1823767be17e198675b8675493ee36826556fba9
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2022-09-20 04:07:59 -0300

    Corregido tema rutas y layout

--- frontEndTpFinal/src/pages/IndexPage.vue
+++ frontEndTpFinal/src/pages/IndexPage.vue
@@ -1,42 +1,26 @@
+<!-- eslint-disable max-len -->
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>
+    <section class="q-mx-lg">
+      <div class="row justify-center items-center">
+        <div class="col-12 col-md-5">
+          <p class="text-h3">Benvenuti a Chihilo</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <q-btn push color="purple" size="lg" class="q-px-lg q-py-xs q-mr-md q-mb-md border-radius-inherit"
+            label="Registrarse" no-caps />
+          <q-btn push color="purple" size="lg" class="q-px-lg q-py-xs q-mb-md border-radius-inherit" label="Registrarse"
+            no-caps />
+        </div>
+        <div class="col-12 col-md-5">
+          <div class="row justify-center">
+            <img src="../assets/car-reserva2.jpg" alt="car reserva" style="height: auto; min-width: 200px;">
+          </div>
+        </div>
+      </div>
+    </section>
   </q-page>
+  <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+    <q-btn fab icon="keyboard_arrow_up" color="accent" />
+  </q-page-scroller>
 </template>
-
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
-</script>
