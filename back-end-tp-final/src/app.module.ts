1a05f253f97662941f4c23e480ce0dd467150af1
venserthesojourner <luchoc2812@gmail.com>
2022-09-20 20:22:20 -0300

    First Backend commit // CREATE entity: usuario // Usario CRUD databaseConfig enviroments endpointConfig

--- back-end-tp-final/src/app.module.ts
+++ back-end-tp-final/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from 'utils/config/database.config';
+import endpointConfig from 'utils/config/endpoint.config';
+import { enviroments } from 'utils/config/enviroments.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        envFilePath: enviroments[process.env.DEVELOPMENT],
+        load: [endpointConfig, databaseConfig],
+        isGlobal: true,
+        validationSchema: 'PLACEHOLDER' ,
+      }
+    ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
--- frontEndTpFinal/src/router/routes.ts
+++ frontEndTpFinal/src/router/routes.ts
@@ -4,9 +4,22 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: 'component',
+        component: () => import('pages/ComponentOnePage.vue'),
+      },
+      {
+        path: 'component',
+        component: () => import('pages/ComponentTwoPage.vue'),
+      },
+    ],
   },
-
+  {
+    path: '/newPath',
+    component: () => import('layouts/CopiesLayout.vue'),
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
