c7670468400a4f52d9965d81f455a00983bc2345
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2022-11-10 19:52:48 -0300

    Actualizando google maps y corrigiendo detalles del backend

--- back-end-tp-final/src/modules/empleado/empleado.module.ts
+++ back-end-tp-final/src/modules/empleado/empleado.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { EmpleadoService } from './empleado.service';
 import { EmpleadoController } from './empleado.controller';
@@ -6,6 +7,6 @@ import { EmpleadoProviders } from './providers/empleado.providers';
 @Module({
   controllers: [EmpleadoController],
   providers: [EmpleadoService, ...EmpleadoProviders],
-  exports: [EmpleadoService]
+  exports: [EmpleadoService],
 })
 export class EmpleadoModule { }
--- frontEndTpFinal/src/pages/BusquedaMobile.vue
+++ frontEndTpFinal/src/pages/BusquedaMobile.vue
@@ -7,37 +7,36 @@
 
 <script>
 import { GoogleMap } from "@capacitor/google-maps";
+import { Geolocation } from '@capacitor/geolocation';
 import { ref, onMounted, onUnmounted, computed } from "vue";
 export default {
   setup() {
     let map = ref(null);
     let coords = ref({ latitude: 0, longitude: 0 });
-    const isSuported = "navigator" in window && "geolocation" in navigator;
     let watcher = null;
-    const currPos = computed(() => ({
-      lat: coords.value.latitude,
-      lng: coords.value.longitude,
-    }));
-
-    console.log(currPos.value);
+    const locations = [
+      {
+        lat: -38.95301138636936,
+        lng: -68.0215979364961,
+      },
+      {
+        lat: -38.96196458229248,
+        lng: -68.02457523815261,
+      },
+      {
+        lat: -38.959461832522436,
+        lng: -68.01440430156816,
+      },
+    ]
+    const printCurrentPosition = async () => {
+      const coordinates = await Geolocation.getCurrentPosition();
+      coords.value.latitude = coordinates.coords.latitude
+      coords.value.longitude = coordinates.coords.longitude
+      console.log(coords.value)
+      console.log('Current position:', coordinates);
+    };
     const createMap = async () => {
       const mapRef = document.getElementById("map");
-
-      const locations = [
-          {
-            lat: -25.361,
-            lng: 131.041,
-          },
-          {
-            lat: -25.367,
-            lng: 131.046,
-          },
-          {
-            lat: -25.363,
-            lng: 131.036,
-          },
-        ]
-
       const newMap = await GoogleMap.create({
         id: "my-map", // Unique identifier for this map instance
         element: mapRef, // reference to the capacitor-google-map element
@@ -45,47 +44,38 @@ export default {
         config: {
           center: {
             // The initial position to be rendered by the map
-            lat: -25.363,
-            lng: 131.044,
+            lat: coords.value.latitude,
+            lng: coords.value.longitude,
           },
-          zoom: 15, // The initial zoom level to be rendered by the map
+          zoom: 3, // The initial zoom level to be rendered by the map
         },
       });
-
       for (let index = 0; index < locations.length; index++) {
         await newMap.addMarker({
-        coordinate: {
-          lat: locations[index].lat,
-          lng: locations[index].lng,
-        },
-        tittle: "you are here bitch!",
-      });
-
+          coordinate: {
+            lat: locations[index].lat,
+            lng: locations[index].lng,
+          },
+          tittle: "you are here bitch!",
+        });
       }
-
       await newMap.setOnMarkerClickListener(() => {
         alert("hi bitch");
       });
-
       await newMap.addMarker({
         coordinate: {
-          lat: -25.363,
-          lng: 131.044,
+          lat: coords.value.latitude,
+          lng: coords.value.longitude,
         },
         tittle: "you are here bitch!",
       });
     };
     onMounted(() => {
-      if (isSuported) {
-        watcher = navigator.geolocation.getCurrentPosition(
-          (position) => (currPos.value = position.coords)
-        );
-        console.log(watcher);
-        console.log(currPos.value);
+      printCurrentPosition();
+      if (!(coords.value.latitude === undefined) || !(coords.value.longitude === undefined)) {
         createMap();
       }
     });
-
     onUnmounted(() => {
       if (watcher) navigator.geolocation.clearWatch(watcher);
     });
